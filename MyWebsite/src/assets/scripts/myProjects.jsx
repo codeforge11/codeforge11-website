7b0e04f1b6b6dbada31553ea208f7671e6245772
codeforge11 <codeforge11@gmail.com>
2026-03-17 21:31:26 +0100

    feat: implement animated project display with framer-motion and add project styling

--- MyWebsite/src/assets/scripts/myProjects.jsx
+++ MyWebsite/src/assets/scripts/myProjects.jsx
@@ -1,12 +1,34 @@
-const myProject = ()=>{
-    return(
-            <div id="projects">
-              <p>&#9679; <a href="https://github.com/codeforge11/SQLMason">SQLMason</a> - Simple and easy-to-use database management tool.</p>
-              <p>&#9679; <a href="https://github.com/codeforge11/CactuDash">CactuDash</a> - Minimalistic and highly optimized dashboard for efficient server management, including Docker and Docker Compose. </p>
-              <p>&#9679; <a href="https://github.com/codeforge11/TFT-DesktopClock">TFT-DesktopClock</a> - Simple digital desk clock built with Arduino and a TFT display using appropriate libraries.</p>
-          </div>
+import { motion } from 'framer-motion';
 
-);
+const projects = [
+    {name:"SQLMason" ,desc:"Simple and easy-to-use database management tool.",iconLink:"", src:"https://github.com/codeforge11/SQLMason", techs:"GO QT"},
+    {name:"CactuDash" ,desc:"Minimalistic and highly optimized dashboard for efficient server management, including Docker and Docker Compose. ",iconLink:"", src:"https://github.com/codeforge11/CactuDash", techs:"GO GIN JS"},
+    {name:"TFT-DesktopClock" ,desc:"Simple digital desk clock built with Arduino and a TFT display using appropriate libraries.",iconLink:"", src:"https://github.com/codeforge11/TFT-DesktopClock", techs:"ARDUINO CPP"},
+]
+
+const myProject = ()=>{
+    return(<>
+            {projects.map((x) => (
+            <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                viewport={{ once: true }}
+                whileInView={{ opacity: 1, y: 0 }}
+                whileHover={{ scale: 1.01, y: -5 }}
+            >
+
+                <div class="projectBox" style={{ display: "flex" }} onClick={() => window.open(x.src, "_blank")}>
+                    <img src={x.iconLink}/>
+
+                    <div class="projectCenter">
+                            <h1>{x.name}</h1>                          
+                            <p>{x.desc}</p>
+                            <p  style={{color:'lightSkyBlue'}}>{x.techs}</p>
+                    </div>
+                </div>
+            </motion.div>
+            ))}
+        </>
+    );
 }
 
 export default myProject
