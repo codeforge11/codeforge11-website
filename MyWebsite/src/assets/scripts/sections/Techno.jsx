1c0ba84f48aad57ccbec78654a0b49f4f22f2826
codeforge11 <codeforge11@gmail.com>
2026-03-23 11:23:32 +0100

    feat: add tehno file

---
+++ MyWebsite/src/assets/scripts/sections/Techno.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { Server, Layout, Cloud, Database, Wrench, Package } from "lucide-react";
+
+const Techno = () =>{
+
+  return(<>
+    
+  </>
+  )
+}
+
+export default Techno;
