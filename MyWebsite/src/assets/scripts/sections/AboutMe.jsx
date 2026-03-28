a40e869d06194bf81ef296456df1d70a6a88c3d1
codeforge11 <codeforge11@gmail.com>
2026-03-28 21:20:30 +0100

    fix: format paragraph for better readability in AboutMe component

--- MyWebsite/src/assets/scripts/sections/AboutMe.jsx
+++ MyWebsite/src/assets/scripts/sections/AboutMe.jsx
@@ -7,9 +7,10 @@ const AboutMe = () => {
           <div className="text">
             <h3>About me:</h3>
             <p>I am a new programmer entering the world of programming, ready to learn new technologies.</p>
-            <p>🌍 I am based in  
-            <span style={{color:'white'}}> Pol</span>
-            <span style={{color:'red'}}>and</span>
+            <p>
+                🌍 I am based in  
+                <span style={{color:'white'}}> Pol</span>
+                <span style={{color:'red'}}>and</span>
             </p>
             <p>🌱 I am currently learning new technologies</p>
             <p>📖 I specialize in solving complex problems</p>
